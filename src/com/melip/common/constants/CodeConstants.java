d3adf82a752fd00a3b6361bb67340764e0c2073d
naoya-suzuki <suzuki0617@gmail.com>
2015-01-26 18:04:21 +0900

    [add]コード用の定数クラスを新規作成

---
+++ src/com/melip/common/constants/CodeConstants.java
@@ -0,0 +1,29 @@
+package com.melip.common.constants;
+
+/**
+ * コードの定数定義用クラスです。
+ */
+public class CodeConstants {
+
+  /**
+   * コンストラクタ
+   */
+  private CodeConstants() {
+    // インスタンス化不可
+  }
+
+  /** ステータス：有効 */
+  public static final String CODE_STS_VALID = "val";
+  /** ステータス：無効 */
+  public static final String CODE_STS_INVALID = "inval";
+  /** ステータス：削除 */
+  public static final String CODE_STS_DELETED = "del";
+  /** 言語区分：共通 */
+  public static final String CODE_LANG_DIV_COMMON = "common";
+  /** 言語区分：共通 */
+  public static final String CODE_LANG_DIV_JAPANESE = "ja";
+  /** 言語区分：共通 */
+  public static final String CODE_LANG_DIV_ENGLISH = "en";
+
+
+}
