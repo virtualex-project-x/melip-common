6fb37d76d2518a287640a27b31ad223ee3cd7c00
naoya-suzuki <suzuki0617@gmail.com>
2015-03-04 18:27:44 +0900

    [modify]コード定義から「エンティティ」を削除

--- src/com/melip/common/constants/CodeConstants.java
+++ src/com/melip/common/constants/CodeConstants.java
@@ -23,13 +23,5 @@ public class CodeConstants {
   public static final String CODE_LANG_DIV_JAPANESE = "ja";
   /** 言語区分：共通 {@value} */
   public static final String CODE_LANG_DIV_ENGLISH = "en";
-  /** エンティティ：地域 {@value} */
-  public static final String CODE_ENTITY_REGION = "reg";
-  /** エンティティ：施設 {@value} */
-  public static final String CODE_ENTITY_FACILITY = "fac";
-  /** エンティティ：施設グループ {@value} */
-  public static final String CODE_ENTITY_FACILITY_GROUP = "facgrp";
-  /** エンティティ：施設_施設グループ_リンク {@value} */
-  public static final String CODE_ENTITY_FACILITY_FACILITY_GROUP_LINK = "facgrpln";
 
 }
