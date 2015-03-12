b81f8bfab4235d00520b276f888637fa02784a53
naoya-suzuki <suzuki0617@gmail.com>
2015-03-12 14:41:31 +0900

    [modify]施設関連情報を追加

--- src/com/melip/common/dto/common/FacilityDto.java
+++ src/com/melip/common/dto/common/FacilityDto.java
@@ -14,10 +14,16 @@ public class FacilityDto extends AbstractDto {
 
   /** 施設IDのフィールド名 */
   public static final String FIELD_FACILITY_ID = "facilityId";
+  /** 地域IDのフィールド名 */
+  public static final String FIELD_REGION_ID = "regionId";
+  /** 公開ステータスのフィールド名 */
+  public static final String FIELD_PUBLISH_STS = "publishSts";
   /** 緯度のフィールド名 */
   public static final String FIELD_LATITUDE = "latitude";
   /** 経度のフィールド名 */
   public static final String FIELD_LONGITUDE = "longitude";
+  /** 親施設IDのフィールド名 */
+  public static final String FIELD_PARENT_FACILITY_ID = "parentFacilityId";
   /** 距離のフィールド名 */
   public static final String FIELD_DISTANCE = "distance";
   /** 属性DTOリストのフィールド名 */
@@ -25,10 +31,16 @@ public class FacilityDto extends AbstractDto {
 
   /** 施設ID */
   private Integer facilityId;
+  /** 地域ID */
+  private Integer regionId;
+  /** 公開ステータス */
+  private String publishSts;
   /** 緯度 */
   private Float latitude;
   /** 経度 */
   private Float longitude;
+  /** 親施設ID */
+  private Integer parentFacilityId;
   /** 距離 */
   private Integer distance;
   /** 属性DTOリスト */
@@ -74,6 +86,42 @@ public class FacilityDto extends AbstractDto {
   }
 
   /**
+   * 地域IDを取得します。
+   * 
+   * @return 地域ID
+   */
+  public Integer getRegionId() {
+    return regionId;
+  }
+
+  /**
+   * 地域IDを設定します。
+   * 
+   * @param regionId 地域ID
+   */
+  public void setRegionId(Integer regionId) {
+    this.regionId = regionId;
+  }
+
+  /**
+   * 公開ステータスを取得します。
+   * 
+   * @return 公開ステータス
+   */
+  public String getPublishSts() {
+    return publishSts;
+  }
+
+  /**
+   * 公開ステータスを設定します。
+   * 
+   * @param publishSts 公開ステータス
+   */
+  public void setPublishSts(String publishSts) {
+    this.publishSts = publishSts;
+  }
+
+  /**
    * 緯度を取得します。
    * 
    * @return 緯度
@@ -107,6 +155,24 @@ public class FacilityDto extends AbstractDto {
    */
   public void setLongitude(Float longitude) {
     this.longitude = longitude;
+  }
+
+  /**
+   * 親施設IDを取得します。
+   * 
+   * @return 親施設ID
+   */
+  public Integer getParentFacilityId() {
+    return parentFacilityId;
+  }
+
+  /**
+   * 親施設IDを設定します。
+   * 
+   * @param parentFacilityId 親施設ID
+   */
+  public void setParentFacilityId(Integer parentFacilityId) {
+    this.parentFacilityId = parentFacilityId;
   }
 
   /**
